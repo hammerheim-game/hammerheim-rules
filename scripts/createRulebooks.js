b03859f9404d94090b5dab107c4e9936af677317
Maxi Clayton Clowes <maxi@codogo.io>
2018-06-21 16:17:11 +0100

    chore(tidyup)

--- scripts/createRulebooks.js
+++ scripts/createRulebooks.js
@@ -1,7 +1,7 @@
 const { handbooker } = require("handbooker");
 
 const options = {
-	debug: true,
+debug: true,
 	style: "./node_modules/handbooker/lib/styles/homebrewery-styles.css",
 	printOptions: {
 		displayHeaderFooter: false
