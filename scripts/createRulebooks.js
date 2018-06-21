7c63faee4e4ddaf9079868d6ebb3ef8e3e1bfcf5
Maxi Clayton Clowes <maxi@codogo.io>
2018-06-21 16:03:38 +0100

    chore(dependencies): updated

--- scripts/createRulebooks.js
+++ scripts/createRulebooks.js
@@ -1,11 +1,11 @@
-const { handbooker, } = require("handbooker");
+const { handbooker } = require("handbooker");
 
 const options = {
-	"debug": true,
-	"style": "./node_modules/handbooker/lib/styles/homebrewery-styles.css",
-	"printOptions": {
-		displayHeaderFooter: false,
-	},
+	debug: true,
+	style: "./node_modules/handbooker/lib/styles/homebrewery-styles.css",
+	printOptions: {
+		displayHeaderFooter: false
+	}
 };
 
 handbooker("./2-core-rules.md", "./rules.pdf", options);
